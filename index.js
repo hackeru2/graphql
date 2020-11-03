5950c107709cf3d54537c6e3f47c01ce32ee8912
amir1004 <amir1004@gmail.com>
2020-11-03 14:28:51 +0200

    b1

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ const server = new ApolloServer({
 });
 
 
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || 5000).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 
 });
